24c1dd175761dc11334871909b5f555bae461e6b
Thomas <Thomas@thomasdye.net>
2025-07-14 16:56:46 +0100

    Initial Commit

---
+++ Picthing/PicthingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicthingApp.swift
+//  Picthing
+//
+//  Created by Thomas Dye on 14/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct PicthingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
